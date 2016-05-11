4f5ad64e374c4c669c37b729ae2e6c905f33210c
Julius Widerberg <julius@adaptivemedia.se>
2016-05-11 10:52:33 +0200

    Base fix

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Julius on 2016-05-11.
+ */
+// include gulp & Sass compiler
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
